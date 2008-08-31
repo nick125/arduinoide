328cbebfc8d21186a1991e9e683d77c3eec8f2f4
Nick Devito <nick@nick125.com>
2008-08-31 16:19:21 -0600

    * Added the header to arduinoide.core.ui.main

--- src/arduinoide/core/ui/main.py
+++ src/arduinoide/core/ui/main.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+#
+# ArduinoIDE Project (http://arduino.bitmeadow.org)
+# (c) 2008 ArduinoIDE Contributors
+# Licensed under the GPLv2 or later. View LICENSE for more information
+#
+
