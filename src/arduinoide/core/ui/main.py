62eb47f5224e62c25e49260964dfeb2c03de25ad
Nick Devito <nick@nick125.com>
2008-08-31 16:28:32 -0600

    * Some more work to arduinoide.core.ui.main. Now it initalizes the window, at least.

--- src/arduinoide/core/ui/main.py
+++ src/arduinoide/core/ui/main.py
@@ -9,4 +9,14 @@ import gtk
 import arduinoide.core.i18n.gt._ as _ 
 
 class MainWindow:
-	def __init__(self, title=):
+	def __init__(self, title=_("Arduino IDE")):
+		"""
+			Initialize the main window.
+		"""
+		self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+	
+	def show(self):
+		"""
+			Place all of your widget show() calls here
+		"""
+		self.window.show()
