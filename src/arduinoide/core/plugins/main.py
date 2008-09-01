8d52a96eb3ac1b46b0dcfa06d4ed1c79a300f535
Nick Devito <nick@nick125.com>
2008-08-31 23:11:58 -0600

    * A little work on arduinoide.core.plugins.main.PluginManager

---
+++ src/arduinoide/core/plugins/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+#
+# ArduinoIDE Project (http://arduino.bitmeadow.org)
+# (c) 2008 ArduinoIDE Contributors
+# Licensed under the GPLv2 or later. View LICENSE for more information
+#
+
+class PluginManager:
+	def __init__(self):
+		self.loaded_plugins = {}
