3a66ddc51cd617a946e181d5014ef46335fecfa4
Nick Devito <nick@nick125.com>
2008-09-01 12:34:16 -0600

    * Added version checks to ArduinoIDE.py

--- src/ArduinoIDE.py
+++ src/ArduinoIDE.py
@@ -6,6 +6,7 @@
 #
 # System imports
 import os
+import sys
 import gtk
 # Our imports
 import arduinoide.core.ui.main
@@ -13,6 +14,19 @@ import arduinoide.core.i18n.gt
 from arduinoide import globals
 
 if __name__ == "__main__":
+	# GTK Version check
+	if gtk.gtk_version[0] < 2 or gtk.gtk_version[1] < 8:
+		ver_dialog = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format="GTK Version Error")
+		ver_dialog.format_secondary_text("Your version of GTK is not compatible with this program. Please upgrade before continuing.")
+		ver_dialog.run()
+		sys.exit()
+	# PyGTK Version check
+	if gtk.pygtk_version[0] < 2 or gtk.pygtk_version[1] < 8:
+		ver_dialog = gtk.MessageDialog(type=gtk.MESSAGE_ERROR, buttons=gtk.BUTTONS_OK, message_format="PyGTK Version Error")
+		ver_dialog.format_secondary_text("Your version of PyGTK is not compatible with this program. Please upgrade before continuing.")
+		ver_dialog.run()
+		sys.exit()
+
 	arduinoide.core.i18n.gt.initialize(os.path.join(globals.RESOURCES_PATH, "mo"))
 	mainwindow = arduinoide.core.ui.main.MainWindow()
 	mainwindow.connect_signals()
