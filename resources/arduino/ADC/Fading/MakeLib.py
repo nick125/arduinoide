dbb227f898c7e6415de3645da97a1a0ddd1d8829
Dale Weber <robotguy@hybotics.com>
2008-09-02 11:48:33 -0700

    Can't remember what I did to this, but have to do this before I can remove it.

--- resources/arduino/ADC/Fading/MakeLib.py
+++ resources/arduino/ADC/Fading/MakeLib.py
@@ -1,28 +1,31 @@
-#!/usr/bin/env python
+!#/usr/bin/env python
 #
 # ArduinoIDE Project (http://arduino.bitmeadow.org)
 # (c) 2008 ArduinoIDE Contributors
 # Licensed under the GPL v2 or later. View LICENSE for more information
 #
-# ArduinoPy module for Python
+# MakeLib module for the Python IDE for Arduino
 #
-# Copy src to dst after writing a header
-def acopyfile(src, dest, header):
+
+#
+# Copy src to dst after writing a header - not yet tested
+def MCopyFile(src, dest, header):
 	line = "";
 
 	# Open files
 	finp = os.open(src, "r");
 	fout = os.open(dest, "w");
 	
-	# Write the header
-	fout.write(header);
+	fout.write(header);			# Write out the header line
+	lcnt = 1;					# Line counter
 
 	# Copy src to dest
 	while (line <> ""):
-		finp.read(line);
+		finp.read(line);		# Get some data
 
-		if (line != ""):
-			fout.write(line);
+		if (line != ""):		# Make sure we have data to write
+			fout.write(line);	# Write the line
+			lcnt += 1;			# Increment line counter
 
 	# Close up shop
 	finp.close();
