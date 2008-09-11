190d9bac3e01ab9b3e90de347541dc408baf6ae0
Nick Devito <nick@nick125.com>
2008-09-11 16:56:11 -0600

    Some work on cleaning up the UI code

--- src/arduinoide/ui/main.py
+++ src/arduinoide/ui/main.py
@@ -15,167 +15,150 @@ import gtksourceview2 as gtksv
 from arduinoide.ui.i18n import _
 
 class MainWindow( gtk.Window ):
-    
-    def __init__(self):
-        """
-            Initialize the main window.
-        """
+	def __init__(self):
+		"""
+			Initialize the main window.
+		"""
 
-        gtk.Window.__init__( self )
+		gtk.Window.__init__(self)
 
-        self.buildGui()
+		self.buildGui()
     
     
-    def buildGui( self ):
-        """
-        This functions builds the main gui
-
-        Arguments:
-        - self: The main object pointer
-        """
+	def buildGui( self ):
+		"""
+			This functions builds the main gui
+		"""
 
         
-        accelGroup = gtk.AccelGroup()
-        self.add_accel_group( accelGroup )
-        self.accelGroup = accelGroup
+		accelGroup = gtk.AccelGroup()
+		self.add_accel_group(accelGroup)
+		self.accelGroup = accelGroup
         
-        self.set_title( "mouseTrap" )
-        self.connect( "destroy", self.close)
+		self.set_title("%s %s")
+		self.connect( "destroy", self.close)
        
-        self.vBox = gtk.VBox()
+		self.vBox = gtk.VBox()
         
-        self.vBox.pack_start( self._buildMenu(), False, False )
-        self.vBox.pack_start( self._buildToolBar(), False, False )
-        self.vBox.pack_start( self._buildMainNoteBook(), False, False )
+		self.vBox.pack_start( self._buildMenu(), False, False )
+		self.vBox.pack_start( self._buildToolBar(), False, False )
+		self.vBox.pack_start( self._buildMainNoteBook(), False, False )
         
-        self._buildVte()
+		self._buildVte()
 
-        self.vBox.show_all()
-        self.add(self.vBox)
-        self.show()
+		self.vBox.show_all()
+		self.add(self.vBox)
+		self.show()
 
-    def _buildMenu( self ):
-        """
-        Builds The application menu
+	def _buildMenu( self ):
+		"""
+			Builds The application menu
+		"""
 
-        Arguments:
-        - self: The main object pointer.
-        """
+		menu = gtk.Menu()
+		mainItems = {}
 
-        menu = gtk.Menu()
-        mainItems = dict()
-
-        menuBar = gtk.MenuBar()
-        menuBar.show()
+		menuBar = gtk.MenuBar()
+		menuBar.show()
         
-        for item in ( _( "File" ), _( "Edit" ), _( "Project" ), _( "View" ), _("Snippets") ):
-            mainItems[ item ] = gtk.MenuItem( item )
-            mainItems[ item ].show()
+		for item in ( _( "File" ), _( "Edit" ), _( "Project" ), _( "View" ), _("Snippets") ):
+			mainItems[item] = gtk.MenuItem(item)
+			mainItems[item].show()
 
 
-        for item in ( _( "File" ), _( "Edit" ), _( "Project" ), _( "View" ), _("Snippets") ):
-            menuBar.append( mainItems[item] )
+		for item in ( _( "File" ), _( "Edit" ), _( "Project" ), _( "View" ), _("Snippets") ):
+			menuBar.append(mainItems[item])
 
-        return menuBar
+		return menuBar
 
-    def _buildToolBar( self ):
-        """
-        Builds The IDE toolbar
+	def _buildToolBar( self ):
+		"""
+			Builds The IDE toolbar
 
-        Arguments:
-        - self: The main object pointer
+			Returns The toolbar.
+		"""
 
-        Returns The toolbar.
-        """
+		handlebox = gtk.HandleBox()
 
-        handlebox = gtk.HandleBox()
-
-        toolbar = gtk.Toolbar()
-        toolbar.set_orientation(gtk.ORIENTATION_HORIZONTAL)
-        toolbar.set_style(gtk.TOOLBAR_BOTH)
-        toolbar.set_border_width(5)
+		toolbar = gtk.Toolbar()
+		toolbar.set_orientation(gtk.ORIENTATION_HORIZONTAL)
+		toolbar.set_style(gtk.TOOLBAR_BOTH)
+		toolbar.set_border_width(5)
         
         
-        toolbar.insert_stock( gtk.STOCK_NEW, _( "Create New File" ), _( "Create New File" ), None, None, -1  ) 
-        toolbar.insert_stock( gtk.STOCK_OPEN, _( "Open File" ), _( "Open File" ), None, None, -1  ) 
-        toolbar.insert_stock( gtk.STOCK_SAVE, _( "Save File" ), _( "Save File" ), None, None, -1  ) 
-        toolbar.append_space()
-        toolbar.insert_stock( gtk.STOCK_EXECUTE, _( "Compile" ), _( "Compile" ), None, None, -1  )
-        toolbar.insert_stock( gtk.STOCK_GO_UP, _( "Upload" ), _( "Upload" ), None, None, -1  )
+		toolbar.insert_stock( gtk.STOCK_NEW, _( "Create New File" ), _( "Create New File" ), None, None, -1  ) 
+		toolbar.insert_stock( gtk.STOCK_OPEN, _( "Open File" ), _( "Open File" ), None, None, -1  ) 
+		toolbar.insert_stock( gtk.STOCK_SAVE, _( "Save File" ), _( "Save File" ), None, None, -1  ) 
+		toolbar.append_space()
+		toolbar.insert_stock( gtk.STOCK_EXECUTE, _( "Compile" ), _( "Compile" ), None, None, -1  )
+		toolbar.insert_stock( gtk.STOCK_GO_UP, _( "Upload" ), _( "Upload" ), None, None, -1  )
 
-#        toolbar.append_widget( self._newStockImageButton( "Upload", gtk.STOCK_GO_UP ), _( "Upload" ), _( "Upload" ) )
-        handlebox.add(toolbar)
+#		toolbar.append_widget( self._newStockImageButton( "Upload", gtk.STOCK_GO_UP ), _( "Upload" ), _( "Upload" ) )
+		handlebox.add(toolbar)
 
 
-        return handlebox
+		return handlebox
 
-    def _buildMainNoteBook( self ):
-        """
-        Builds the main notebook of the ide
+	def _buildMainNoteBook( self ):
+		"""
+			Builds the main notebook of the ide
+			Returns The notebook.
+		"""
 
-        Arguments:
-        - self: The main object pointer
-
-        Returns The notebook.
-        """
-
-        self.notebook = gtk.Notebook()
-        self.notebook.set_tab_pos( gtk.POS_TOP )
+		self.notebook = gtk.Notebook()
+		self.notebook.set_tab_pos( gtk.POS_TOP )
         
-        tab =  gtksv.View()
-        tab.set_show_line_numbers( True ) 
+		tab =  gtksv.View()
+		tab.set_show_line_numbers( True ) 
         
-        self.notebook.append_page( tab, gtk.Label( _( "New Tab" ) ) )
-        self.notebook.show()
+		self.notebook.append_page( tab, gtk.Label( _( "New Tab" ) ) )
+		self.notebook.show()
 
-        return self.notebook
+		return self.notebook
 
-    def _buildVte( self ):
-        """
-        Builds a VTE
-
-        Arguments:
-        - self: The main object pointer.
-        """
+	def _buildVte( self ):
+		"""
+			Builds a VTE
+		"""
         
-        vt = vte.Terminal ()
-        vt.connect ("child-exited", lambda term: gtk.main_quit())
-        vt.fork_command()
+		vt = vte.Terminal ()
+		vt.connect("child-exited", lambda term: gtk.main_quit())
+		vt.fork_command()
 
-        self.notebook.append_page( vt, gtk.Label( "VTE" ) )
+		self.notebook.append_page( vt, gtk.Label( "VTE" ) )
     
-    def _newStockImageButton( self, label, stock ):
-        """
-        Creates an image button from gtk's stock.
+	def _newStockImageButton( self, label, stock ):
+		"""
+			Creates an image button from gtk's stock.
         
-        Arguments:
-        - self: The main object pointer
-        - label: The buttons label
-        - stock: The Stock image the button will use. E.g: gtk.STOCK_GO-FORWARD
+			Arguments:
+			- self
+			- label: The buttons label
+			- stock: The Stock image the button will use. E.g: gtk.STOCK_GO-FORWARD
         
-        Returns buttonLabelBox A gtk.HBox that contains the new image stock button.
-        """
+			Returns buttonLabelBox - A gtk.HBox that contains the new image stock button.
+		"""
         
-        buttonLabelBox = gtk.HBox()
+		buttonLabelBox = gtk.HBox()
+       
+		im = gtk.image_new_from_stock( stock, gtk.ICON_SIZE_BUTTON )
+       
+		label = gtk.Label( label )
+		label.set_alignment( 0.0, 0.5 )
+		label.set_use_underline( True )
         
-        im = gtk.image_new_from_stock( stock, gtk.ICON_SIZE_BUTTON )
+		buttonLabelBox.pack_start( im )
+		buttonLabelBox.pack_start( label )
+		buttonLabelBox.show_all()
         
-        label = gtk.Label( label )
-        label.set_alignment( 0.0, 0.5 )
-        label.set_use_underline( True )
-        
-        buttonLabelBox.pack_start( im )
-        buttonLabelBox.pack_start( label )
-        buttonLabelBox.show_all()
-        
-        return buttonLabelBox
+		return buttonLabelBox
 
-    def close( self, *args ):
-        """
-        Close Function for the quit button.
+	def close( self, *args ):
+		"""
+			Close Function for the quit button.
         
-        Arguments:
-        - self: The main object pointer.
-        - *args: The widget callback arguments.
-        """
-        print "close"
+			Arguments:
+			- self: The main object pointer.
+			- *args: The widget callback arguments.
+		"""
+		print "close"
