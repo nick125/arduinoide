99f07401913aa5d2970e02f31709d93b12f92ec4
Nick Devito <nick@nick125.com>
2008-09-13 05:03:46 -0600

    * Added the files for arduinoide.core.config

---
+++ src/arduinoide/core/config/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+#
+# ArduinoIDE Project (http://arduino.bitmeadow.org)
+# (c) 2008 ArduinoIDE Contributors
+# Licensed under the GPLv2 or later. View LICENSE for more information
+#
+
+
